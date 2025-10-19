0ef59d998a98d31be3a1b600d19550ca97f2e27d
Parker Rowland <parkertrowland@gmail.com>
2025-10-19 14:29:32 -0400

    Re-added Skills resume section

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -59,6 +59,7 @@ export interface TranslationData {
             degree: TranslationEntry;
             highlights: TranslationEntry[];
         }[];
+        skills: TranslationEntry[];
     }
     gems?: {
         gemsData: TranslationEntry;
